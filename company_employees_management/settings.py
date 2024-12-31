3cd7d37b29af1bdc127fd6c1689082d89b8fe04c
Tejas Ekhande <tejasekhande01@gmail.com>
2024-12-31 17:01:07 +0530

    install django rest framework

--- company_employees_management/settings.py
+++ company_employees_management/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+EXTERNAL_APPS = [
+    'rest_framework'
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
